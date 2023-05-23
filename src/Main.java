e781229395e2aa58bbaa505311b2655d39a4de25
Yanka Santos <yanka.santos@thoughtworks.com>
2023-05-23 18:54:53 -0300

    reset of exemples

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        //insert text line into a variable
-        System.out.println("Digite seu nome, sobrenome e idade");
-        Scanner textInput = new Scanner(System.in);
 
-        int age = textInput.nextInt();
-        textInput.nextLine();
-        String username = textInput.nextLine();
-        String lastName = textInput.nextLine();
-
-
-        System.out.println("Dados do usuário:");
-        System.out.println(username+ " " + lastName);
-        System.out.println(age);
-
-        textInput.close();
     }
 }
